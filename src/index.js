00ab9e0fbf5ed34714ec9575da87918456aadbae
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-10-30 22:54:10 +0330

    Make Project RTL Support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App/App';
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App/App";
+import "./index.css";
+// ---------------------
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+// ---------------------
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const cacheRtl = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+});
+const theme = createTheme({
+    direction: "rtl",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <CacheProvider value={cacheRtl}>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </CacheProvider>
+    </React.StrictMode>
 );
