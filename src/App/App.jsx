69e610d4f071e4dbeef361294e61b76eb29f8126
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-10-30 21:06:48 +0330

    install and config tailwind

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import "@fontsource/vazirmatn";
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+    <h1 className="text-3xl font-bold underline text-blue-700">
+      صورتجساب
+    </h1>
+    </div>
+  );
+}
+
+export default App;
