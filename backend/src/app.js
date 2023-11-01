da9e14acda71664eba4e9d1fb97e9d6fb34444f2
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-11-01 09:47:14 +0330

    Backend : FIX access bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const invoicesRouter = require('./routes/invoices');
+const cors = require('cors'); // Import the cors package
+
+// Define ANSI escape codes for text color
+const colors = {
+  reset: "\x1b[0m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  cyan: "\x1b[36m",
+};
 
 const app = express();
 const port = 3001;
+const path = "api"
+
+app.use(cors()); // Enable CORS for all routes
 
 app.use(bodyParser.json());
 
-app.use('/api', invoicesRouter);
+app.use(`/${path}`, invoicesRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  
+  console.log(`${colors.yellow}Server is running on port ${port}  ${colors.reset}`);
+  console.log(`${colors.green}Success: http://localhost:${port}/${path} ${colors.reset}`);
 });
